ec20fe3c9b2cdbf84ee74f0d13c29a158dd3da45
Sarah Good <sarah.elizabeth.good@gmail.com>
2025-01-31 16:49:57 -0800

    enable read/unread status

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 const myLibrary = [];
 const libContainer = document.getElementById('book-container');
 
-function Book(title, author, summary) {
+function Book(title, author, summary, read=false) {
   this.title = title;
   this.author = author;
   this.summary = summary;
+  this.read = read;
 }
 
-function addBookToLibrary(title, author, summary) {
-    var newBook = new Book(title, author, summary);
+function addBookToLibrary(title, author, summary, read=false) {
+    var newBook = new Book(title, author, summary, read);
     myLibrary.push(newBook);
-    console.log(newBook);
-    console.log(myLibrary);
 }
 
 function listLibrary(lib) {
@@ -22,15 +21,23 @@ function listLibrary(lib) {
         title = document.createElement('h2');
         author = document.createElement('h3');
         summary = document.createElement('p');
+        read = document.createElement('div');
 
         div.className = 'card';
         title.innerHTML += lib[i].title;
         author.innerHTML += lib[i].author;
         summary.innerHTML += lib[i].summary;
 
+        if (lib[i].read === true) {
+            read.innerHTML = "Read";
+        } else {
+            read.innerHTML = "Unread";
+        }
+
         div.appendChild(title);
         div.appendChild(author);
         div.appendChild(summary);
+        div.appendChild(read);
 
         libContainer.appendChild(div);
 
@@ -40,15 +47,18 @@ function listLibrary(lib) {
 
 document.getElementById('submit').addEventListener("click", function() {
     event.preventDefault();
+    libContainer.innerHTML = "";
+
     let title = document.getElementById('bTitle').value;
     let author = document.getElementById('bAuthor').value;
     let summary = document.getElementById('bSummary').value;
+    let read = document.getElementById('bRead').checked;
 
     if (title === "" || author === "" || summary === "") {
         alert("Must fill out all fields.");
     } else {
         document.getElementById("newbookform").reset();
-        addBookToLibrary(title, author, summary);
+        addBookToLibrary(title, author, summary, read);
         listLibrary(myLibrary);
     }
 })
